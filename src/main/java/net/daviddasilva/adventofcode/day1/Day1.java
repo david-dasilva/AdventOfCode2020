a1c8111861ef688d2beb7cf4e6186df4f5e76a1d
David Da Silva <david.dasilva@zenika.com>
2020-12-01 19:31:02 +0100

    Day 1 part 2 solved

--- src/main/java/net/daviddasilva/adventofcode/day1/Day1.java
+++ src/main/java/net/daviddasilva/adventofcode/day1/Day1.java
@@ -9,7 +9,8 @@ public class Day1 {
 
         Day1 day1 = new Day1();
 
-        System.out.println(day1.solvePart1(filename));
+        System.out.println("Part 1 : " + day1.solvePart1(filename));
+        System.out.println("Part 2 : " + day1.solvePart2(filename));
     }
 
     public int solvePart1(String filename) {
@@ -20,6 +21,22 @@ public class Day1 {
             for (Integer b : data) {
                 if (a + b == 2020) {
                     return a * b;
+                }
+            }
+        }
+
+        throw new IllegalArgumentException("Could not solve solution");
+    }
+
+    public int solvePart2(String filename) {
+        var data = ReadUtils.asIntList(filename);
+
+        for (Integer a : data) {
+            for (Integer b : data) {
+                for (Integer c : data) {
+                    if (a + b + c == 2020) {
+                        return a * b * c;
+                    }
                 }
             }
         }
--- src/test/java/net/daviddasilva/adventofcode/day1/Day1Test.java
+++ src/test/java/net/daviddasilva/adventofcode/day1/Day1Test.java
@@ -7,10 +7,18 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class Day1Test {
 
     @Test
-    void shouldSolveExample() {
+    void shouldSolveExample1() {
         var filename = "/day1/example1.txt";
 
         var day1 = new Day1();
         assertThat(day1.solvePart1(filename)).isEqualTo(514579);
+    }
+
+    @Test
+    void shouldSolveExample2() {
+        var filename = "/day1/example1.txt";
+
+        var day1 = new Day1();
+        assertThat(day1.solvePart2(filename)).isEqualTo(241861950);
     }
 }
