2d514e00eb0892f3309a61f91b8f26eef7ecd017
David Da Silva <david.dasilva@zenika.com>
2020-12-03 13:35:28 +0100

    Improve Day 1 and add test on validated solutions

--- src/main/java/net/daviddasilva/adventofcode/day1/Day1.java
+++ src/main/java/net/daviddasilva/adventofcode/day1/Day1.java
@@ -1,25 +1,40 @@
 package net.daviddasilva.adventofcode.day1;
 
 import net.daviddasilva.adventofcode.utils.ReadUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
 
 public class Day1 {
 
-    public static void main(String[] args) {
-        var filename = "/day1/input.txt";
+    private static final Logger logger = LoggerFactory.getLogger(Day1.class);
 
-        Day1 day1 = new Day1();
+    private final List<Integer> data;
 
-        System.out.println("Part 1 : " + day1.solvePart1(filename));
-        System.out.println("Part 2 : " + day1.solvePart2(filename));
+    public Day1(String filename) {
+        this.data = ReadUtils.asIntList(filename);
     }
 
-    public int solvePart1(String filename) {
+    public static void main(String[] args) {
+        Day1 day1 = new Day1("/day1/input.txt");
 
-        var data = ReadUtils.asIntList(filename);
+        logger.info("Part 1 Solution \t⭐\t\t{} ", day1.solvePart1());
+        logger.info("Part 1 Solution \t⭐⭐\t{}", day1.solvePart1());
+    }
+
+    public int solvePart1() {
+        var checks = 0;
+        long start = System.nanoTime();
 
         for (Integer a : data) {
             for (Integer b : data) {
+                if (b.equals(a)) {
+                    continue;
+                }
+                checks++;
                 if (a + b == 2020) {
+                    logger.debug("Solved part 1 in {} checks and {} ms", checks, (System.nanoTime() - start) / 1000000);
                     return a * b;
                 }
             }
@@ -28,13 +43,23 @@ public class Day1 {
         throw new IllegalArgumentException("Could not solve solution");
     }
 
-    public int solvePart2(String filename) {
-        var data = ReadUtils.asIntList(filename);
+    public int solvePart2() {
+        var checks = 0;
+        long start = System.nanoTime();
+
 
         for (Integer a : data) {
             for (Integer b : data) {
+                if (b.equals(a)) {
+                    continue;
+                }
                 for (Integer c : data) {
+                    if (c.equals(b) || c.equals(a)) {
+                        continue;
+                    }
+                    checks++;
                     if (a + b + c == 2020) {
+                        logger.debug("Solved part 2 in {} checks and {} ms", checks, (System.nanoTime() - start) / 1000000);
                         return a * b * c;
                     }
                 }
--- src/test/java/net/daviddasilva/adventofcode/day1/Day1Test.java
+++ src/test/java/net/daviddasilva/adventofcode/day1/Day1Test.java
@@ -10,15 +10,32 @@ public class Day1Test {
     void shouldSolveExample1() {
         var filename = "/day1/example1.txt";
 
-        var day1 = new Day1();
-        assertThat(day1.solvePart1(filename)).isEqualTo(514579);
+        var day1 = new Day1(filename);
+        assertThat(day1.solvePart1()).isEqualTo(514579);
     }
 
     @Test
     void shouldSolveExample2() {
         var filename = "/day1/example1.txt";
 
-        var day1 = new Day1();
-        assertThat(day1.solvePart2(filename)).isEqualTo(241861950);
+        var day1 = new Day1(filename);
+        assertThat(day1.solvePart2()).isEqualTo(241861950);
+    }
+
+
+    @Test
+    void shouldSolvePart1() {
+        var filename = "/day1/input.txt";
+
+        var day1 = new Day1(filename);
+        assertThat(day1.solvePart1()).isEqualTo(786811);
+    }
+
+    @Test
+    void shouldSolvePart2() {
+        var filename = "/day1/input.txt";
+
+        var day1 = new Day1(filename);
+        assertThat(day1.solvePart2()).isEqualTo(199068980);
     }
 }
