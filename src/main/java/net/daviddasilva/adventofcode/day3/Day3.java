5f57523077c5e0356e57aca2ccf0b1c23fbb4f6b
David Da Silva <david.dasilva@zenika.com>
2020-12-03 12:11:14 +0100

    Day 3 part 2 solved

--- src/main/java/net/daviddasilva/adventofcode/day3/Day3.java
+++ src/main/java/net/daviddasilva/adventofcode/day3/Day3.java
@@ -7,26 +7,43 @@ import java.util.List;
 public class Day3 {
 
     public static char TREE = '#';
+    private final String filename;
 
     public static void main(String[] args) {
-        var filename = "/day3/input.txt";
+        var day3 = new Day3("/day3/input.txt");
 
-        var day3 = new Day3();
-
-        System.out.println("Part 1 : " + day3.solvePart1(filename));
+        System.out.println("Part 1 : " + day3.solvePart1());
+        System.out.println("Part 2 : " + day3.solvePart2());
     }
 
-    public int solvePart1(String filename) {
+    public Day3(String filename) {
+        this.filename = filename;
+    }
+
+    public long solvePart1() {
         // right 3 down 1
+        return ride( 3, 1);
+    }
+
+    public long solvePart2() {
+        return ride(1, 1)
+                * ride(3, 1)
+                * ride(5, 1)
+                * ride(7, 1)
+                * ride(1, 2);
+    }
+
+    long ride(int right, int down) {
         List<String> lines = ReadUtils.asStringList(filename);
         var startPosition = 0;
         var trees = 0;
 
         System.out.println(lines.get(0)); // for pretty display
 
-        for (int i = 1; i < lines.size(); i++) {
-            startPosition += 3;
-            if (getElementAtPosition(lines.get(i), startPosition) == TREE) {
+        for (int i = 0; i < lines.size() -down; i = i+down) {
+            startPosition += right;
+            var lineToRead = i+down;
+            if (getElementAtPosition(lines.get(lineToRead), startPosition) == TREE) {
                 trees++;
             }
         }
--- src/test/java/net/daviddasilva/adventofcode/day3/Day3Test.java
+++ src/test/java/net/daviddasilva/adventofcode/day3/Day3Test.java
@@ -6,56 +6,89 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class Day3Test {
 
+    String filename = "/day3/example1.txt";
+
     @Test
     void shouldSolveExample1() {
-        var filename = "/day3/example1.txt";
+        var day3 = new Day3(filename);
 
-        var day3 = new Day3();
+        assertThat(day3.solvePart1()).isEqualTo(7);
+    }
 
-        assertThat(day3.solvePart1(filename)).isEqualTo(7);
+    @Test
+    void shouldSolveExample2() {
+        var day3 = new Day3(filename);
+
+        assertThat(day3.solvePart2()).isEqualTo(336);
+    }
+
+    @Test
+    void shouldSolveRide1Right1Down() {
+        var day3 = new Day3(filename);
+
+        assertThat(day3.ride(1, 1)).isEqualTo(2);
+    }
+
+    @Test
+    void shouldSolveRide3Right1Down() {
+        var day3 = new Day3(filename);
+
+        assertThat(day3.ride(3, 1)).isEqualTo(7);
+    }
+
+    @Test
+    void shouldSolveRide5Right1Down() {
+        var day3 = new Day3(filename);
+
+        assertThat(day3.ride(5, 1)).isEqualTo(3);
+    }
+
+    @Test
+    void shouldSolveRide7Right1Down() {
+        var day3 = new Day3(filename);
+
+        assertThat(day3.ride(7, 1)).isEqualTo(4);
+    }
+
+    @Test
+    void shouldSolveRide1Right2Down() {
+        var day3 = new Day3(filename);
+
+        assertThat(day3.ride(1, 2)).isEqualTo(2);
     }
 
     @Test
     void shouldGetTheCharAtPositionWithinLine() {
         var line = "..#........";
-        var day3 = new Day3();
+        var day3 = new Day3(filename);
         assertThat(day3.getElementAtPosition(line, 2)).isEqualTo('#');
     }
 
     @Test
     void shouldGetTheCharAtPositionByLoopingTheLineOneTime() {
         var line = "..#........";
-        var day3 = new Day3();
+        var day3 = new Day3(filename);
         assertThat(day3.getElementAtPosition(line, 13)).isEqualTo('#');
     }
 
     @Test
     void shouldGetTheCharAtPositionByLoopingTheLineMultipleTimes() {
         var line = "..#........";
-        var day3 = new Day3();
+        var day3 = new Day3(filename);
         assertThat(day3.getElementAtPosition(line, 24)).isEqualTo('#');
     }
 
     @Test
     void shouldGetTheCharAtPositionCaseLastChar() {
         var line = "..........#";
-        var day3 = new Day3();
+        var day3 = new Day3(filename);
         assertThat(day3.getElementAtPosition(line, 10)).isEqualTo('#');
     }
 
     @Test
     void shouldGetTheCharAtPositionCasePosIsLineLenght() {
         var line = "#..........";
-        var day3 = new Day3();
+        var day3 = new Day3(filename);
         assertThat(day3.getElementAtPosition(line, line.length())).isEqualTo('#');
     }
-
-/*    @Test
-    void shouldSolveExample2() {
-        var filename = "/day3/example1.txt";
-
-        var day3 = new Day3();
-
-        assertThat(day3.solvePart2(filename)).isEqualTo(1);
-    }*/
 }
