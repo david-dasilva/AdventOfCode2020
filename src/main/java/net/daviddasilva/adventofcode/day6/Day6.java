1813fce884d96390e5274598e20956386d9ffb65
David Da Silva <david.dasilva@zenika.com>
2020-12-06 14:28:46 +0100

    Day 6 part 2 solved

--- src/main/java/net/daviddasilva/adventofcode/day6/Day6.java
+++ src/main/java/net/daviddasilva/adventofcode/day6/Day6.java
@@ -1,6 +1,5 @@
 package net.daviddasilva.adventofcode.day6;
 
-import net.daviddasilva.adventofcode.day4.Day4;
 import net.daviddasilva.adventofcode.utils.ReadUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,7 +7,6 @@ import org.slf4j.LoggerFactory;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 public class Day6 {
     private static final Logger logger = LoggerFactory.getLogger(Day6.class);
@@ -19,7 +17,7 @@ public class Day6 {
         Day6 day6 = new Day6("/day6/input.txt");
 
         logger.info("Part 1 Solution \t⭐\t\t{} ", day6.solvePart1());
-        //logger.info("Part 2 Solution \t⭐⭐\t{}", day6.solvePart2());
+        logger.info("Part 2 Solution \t⭐⭐\t{}", day6.solvePart2());
     }
 
 
@@ -28,17 +26,54 @@ public class Day6 {
     }
 
     public long solvePart1() {
-        return extractGroupResponses().stream()
+        return countDistinctResponses().stream()
                 .reduce(0L, Long::sum);
     }
 
 
-    List<Long> extractGroupResponses() {
+    public long solvePart2() {
+        return countCommonResponses().stream()
+                .reduce(0L, Long::sum);
+    }
+
+    List<Long> countDistinctResponses() {
         return Arrays.stream(data.split("\\n\\n"))
                 .map(group -> group.replaceAll("[\n\r]", ""))
-                .map(String::chars)
-                .map(chars -> chars.distinct().count())
+                .map(Day6::countDistinctResponseForGroup)
                 .collect(Collectors.toList());
     }
 
+    static Long countDistinctResponseForGroup(String group) {
+        return group.chars().distinct().count();
+    }
+
+    List<Long> countCommonResponses() {
+        return Arrays.stream(data.split("\\n\\n"))
+                .map(Day6::countCommonResponseForGroup)
+                .collect(Collectors.toList());
+    }
+
+    static long countCommonResponseForGroup(String group) {
+        var answers = group.lines()
+                .map(Day6::sortAnswer)
+                .collect(Collectors.toList());
+        var nbOfAnswers = answers.size();
+        if (nbOfAnswers == 1) {
+            return answers.get(0).length();
+        }
+
+        String commonChars = answers.get(0);
+        for (int i = 1; i < answers.size(); i++) {
+            var commonAnswerWithPrevious = answers.get(i).replaceAll("[^" + answers.get(i-1) + "]", "");
+            if (!commonChars.isEmpty()) {
+                commonChars = commonAnswerWithPrevious.replaceAll("[^" + commonChars + "]", "");
+            }
+        }
+
+        return countDistinctResponseForGroup(commonChars);
+    }
+
+    static String sortAnswer(String answer) {
+        return answer.chars().sorted().collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append).toString();
+    }
 }
--- src/test/java/net/daviddasilva/adventofcode/day6/Day6Test.java
+++ src/test/java/net/daviddasilva/adventofcode/day6/Day6Test.java
@@ -34,15 +34,54 @@ public class Day6Test {
         }
 
         @Test
-        void shouldCountResponsesPerGroups() {
+        void shouldCountDistinctResponsesPerGroups() {
 
             var filename = "/day6/example1.txt";
             var day6 = new Day6(filename);
-            var actual = day6.extractGroupResponses();
+            var actual = day6.countDistinctResponses();
             assertThat(actual.size()).isEqualTo(5);
             assertThat(actual.get(0)).isEqualTo(3);
             assertThat(actual.get(1)).isEqualTo(3);
             assertThat(actual.get(2)).isEqualTo(3);
+            assertThat(actual.get(3)).isEqualTo(1);
+            assertThat(actual.get(4)).isEqualTo(1);
+        }
+    }
+
+
+    @DisplayName("For part 2")
+    @Nested
+    class Part2 {
+
+        @DisplayName("Should solve example 2")
+        @Test
+        void shouldSolveExample2() {
+
+            var filename = "/day6/example1.txt";
+            var day6 = new Day6(filename);
+
+            assertThat(day6.solvePart2()).isEqualTo(6);
+        }
+
+        @DisplayName("It should solve to validated solution")
+        @Test
+        void shouldSolvePart2() {
+            var filename = "/day6/input.txt";
+            var day6 = new Day6(filename);
+
+            assertThat(day6.solvePart2()).isEqualTo(3406);
+        }
+
+
+        @Test
+        void shouldCountCommonResponsePerGroup() {
+            var filename = "/day6/example1.txt";
+            var day6 = new Day6(filename);
+            var actual = day6.countCommonResponses();
+            assertThat(actual.size()).isEqualTo(5);
+            assertThat(actual.get(0)).isEqualTo(3);
+            assertThat(actual.get(1)).isEqualTo(0);
+            assertThat(actual.get(2)).isEqualTo(1);
             assertThat(actual.get(3)).isEqualTo(1);
             assertThat(actual.get(4)).isEqualTo(1);
         }
